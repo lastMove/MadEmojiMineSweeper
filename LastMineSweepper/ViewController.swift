a94bff3516dfd801297c559fcb2d880f41601e84
lastMove <lastMove@me.com>
2017-03-09 23:21:20 +0100

    Initial Commit

---
+++ LastMineSweepper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LastMineSweepper
+//
+//  Created by jason akakpo on 09/03/2017.
+//  Copyright © 2017 MTT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
